fb686316967e940a6f0f52e4ac0c0bd1018827e6
radoslavnaya-coder <killaarte13@gmail.com>
2024-02-29 17:49:14 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <Header />
+</template>
+<style scoped>
+@import "./assets/main.css";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup>
+import MaterialSymbols_searchVue from "./icons/Material-symbols_search.vue";
+</script>
+<template>
+    <header>
+        <div>
+            <nav>
+                <img src="@/assets/logo.png" width="118" height="86">
+                <input type="search" name="main-search">
+                <label for="main-search"><MaterialSymbols_searchVue /></label>
+                <button></button>
+                <img src="@/assets/user-img.png">
+            </nav>
+        </div>
+    </header>
+</template>
+<style scoped>
+input{
+    outline: none;
+}
+input::-webkit-search-cancel-button{
+    display: none !important;
+}
+</style>
