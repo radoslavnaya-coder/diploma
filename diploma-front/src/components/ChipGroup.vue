95fe472c6b2a257fb33e85048787827938b65735
radoslavnaya-coder <killaarte13@gmail.com>
2024-05-19 16:41:06 +0400

    add arrows, categories

---
+++ diploma-front/src/components/ChipGroup.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-chip-group selected-class="orange" show-arrows multiple>
+    <v-chip
+      v-for="category in categories"
+      :key="category.id"
+      class="outlined-orange"
+      variant="outlined"
+    >
+      {{ category.name }}
+    </v-chip>
+  </v-chip-group>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+import { instance } from "@/components/axios/instance"
+
+export default {
+  setup() {
+    const categories = ref()
+    onMounted(() => {
+    instance.get('/allCategory').then((res) => {
+    categories.value = res.data
+    console.log('Data here:', res.data)
+    })
+    })
+    return { categories }
+  },
+  data: () => ({
+    tags: [
+      "Еда",
+      "Своими руками",
+      "Vacation",
+      "Food",
+      "Drawers",
+      "Shopping",
+      "Art",
+      "Tech",
+      "Creative Writing",
+      "Еда",
+      "Своими руками",
+      "Vacation",
+      "Food",
+      "Drawers",
+      "Shopping",
+      "Art",
+      "Tech",
+      "Creative Writing"
+    ],
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+.v-chip {
+    border-radius: 0 !important;
+    padding: 1.3rem 1.5rem !important;
+    font-size: 18px !important;
+    margin: 1rem 5px !important;
+  }
+  .orange {
+    background: #e36238 !important;
+    border: 1px solid #e36238;
+    color: white !important;
+  }
+  .outlined-orange {
+    background: none;
+    border: 1px solid #e36238;
+    color: #7e2513;
+  }
+</style>
+
+<style lang="scss">
+.mdi-chevron-left {
+    background: url(@/assets/images/LeftArrow.svg);
+}
+.mdi-chevron-right {
+    background: url(@/assets/images/LeftArrow.svg);
+    rotate: 180deg;
+}
+</style>
--- diploma-front/src/views/HomeView.vue
+++ diploma-front/src/views/HomeView.vue
@@ -1,18 +1,8 @@
 <template>
   <div>
   <Header />
-  {{ categories }}
   <main>
-    <v-chip-group selected-class="orange" show-arrows multiple>
-      <v-chip
-        v-for="tag in tags"
-        :key="tag"
-        class="outlined-orange"
-        variant="outlined"
-      >
-        {{ tag }}
-      </v-chip>
-    </v-chip-group>
+    <Chip />
     <ImagesGrid />
   </main>
 </div>
@@ -21,71 +11,12 @@
 <script setup>
 import Header from "../components/Header.vue"
 import ImagesGrid from "../components/imagesGrid.vue"
-</script>
-
-<script>
-import { ref } from 'vue'
-import { instance } from "@/components/axios/instance"
-
-export default {
-  setup() {
-    const categories = ref([])
-    const getCategories = async () => {
-      try {
-        const response = await instance.get('/allCategory')
-          categories.value = response.data
-      } catch (err) {
-        throw new Error(err)
-      }
-    }
-    return {
-      categories, getCategories
-    }
-  },
-  data: () => ({
-    tags: [
-      "Еда",
-      "Своими руками",
-      "Vacation",
-      "Food",
-      "Drawers",
-      "Shopping",
-      "Art",
-      "Tech",
-      "Creative Writing",
-      "Еда",
-      "Своими руками",
-      "Vacation",
-      "Food",
-      "Drawers",
-      "Shopping",
-      "Art",
-      "Tech",
-      "Creative Writing"
-    ],
-  }),
-};
+import Chip from "../components/ChipGroup.vue"
 </script>
 
 <style lang="scss" scoped>
 main {
   width: 100vw;
   padding: 1rem 12rem 2rem 14rem;
-}
-.v-chip {
-  border-radius: 0 !important;
-  padding: 1.3rem 1.5rem !important;
-  font-size: 18px !important;
-  margin: 1rem 5px !important;
-}
-.orange {
-  background: #e36238 !important;
-  border: 1px solid #e36238;
-  color: white !important;
-}
-.outlined-orange {
-  background: none;
-  border: 1px solid #e36238;
-  color: #7e2513;
 }
 </style>
