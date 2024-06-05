b0b185d3a7100b09650195b2b2afbccacd965d8b
radoslavnaya-coder <killaarte13@gmail.com>
2024-06-05 23:22:20 +0400

    if publication is empty

--- diploma-front/src/components/imagesGridForUser.vue
+++ diploma-front/src/components/imagesGridForUser.vue
@@ -1,11 +1,16 @@
 <template>
-  <Masonry-wall :items="posts" :ssr-columns="1" :column-width="240" :gap="10">
-    <template #default="{ item }">
-      <div style="max-height: 59rem">
-        <img width="240" :src="`${item.img}`" alt="image" />
-      </div>
-    </template>
-  </Masonry-wall>
+  <div v-if="posts[0] != null">
+    <Masonry-wall :items="posts" :ssr-columns="1" :column-width="240" :gap="10">
+      <template #default="{ item }">
+        <div style="max-height: 59rem">
+          <img width="240" :src="`${item.img}`" alt="image" />
+        </div>
+      </template>
+    </Masonry-wall>
+  </div>
+  <div class="not-empty" v-else>
+    <h3>Публикаций нет. Сделайте свой первый пост!</h3>
+  </div>
 </template>
 
 <script>
@@ -17,9 +22,10 @@ export default {
     const posts = ref([]);
     const token = localStorage.getItem("token");
     onMounted(() => {
-      instance.get("/allPostsUser", {
+      instance
+        .get("/allPostsUser", {
           headers: {
-            "Authorization": "Bearer " + token,
+            Authorization: "Bearer " + token,
           },
         })
         .then((res) => {
@@ -30,3 +36,13 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.not-empty {
+  text-align: center;
+  h3 {
+  font-weight: 400;  
+  color: #7E2513;
+  }
+}
+</style>
